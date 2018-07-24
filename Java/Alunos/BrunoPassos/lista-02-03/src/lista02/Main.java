dfa6aa5806c37ff6c7d3dcb6a572a5022d16c4b3
Bruno Passos <brunopassos133@hotmail.com>
2018-07-24 15:07:58 -0300

    Exercicio 04

---
+++ Java/Alunos/BrunoPassos/lista-02-03/src/lista02/Main.java
@@ -0,0 +1,32 @@
+package lista02;
+
+import java.io.Console;
+
+public class Main {
+	// 3. Construa um programa que leia do console 3 números inteiros, e imprima o
+	// resultado em ordem crescente
+
+	public static void main(String[] args) {
+
+		Console console = System.console();
+
+		// Leitura do console
+		System.out.println("Informe 3 valores: ( pressione enter para cada valor informado");
+		String v1 = console.readLine();
+		String v2 = console.readLine();
+		String v3 = console.readLine();
+
+		int i1, i2, i3;
+
+		// conversao para int
+		i1 = Integer.parseInt(v1);
+		i2 = Integer.parseInt(v2);
+		i3 = Integer.parseInt(v3);
+
+		// determinar ordem
+		
+		
+
+	}
+
+}
