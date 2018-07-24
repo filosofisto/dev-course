c591d0d614d99a8aca00d9ae3b498903770409a6
Lenilson Florencio <lenilson.florencio@cpb.org.br>
2018-07-24 15:12:13 -0300

    Lenilson - Ex05

--- Java/Alunos/Lenilson/lista-02-05/src/Main.java
+++ Java/Alunos/Lenilson/lista-02-05/src/Main.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner ler = new Scanner(System.in);
+		
+		//Leitura do Console
+		System.out.println("Informe a nota da prova 1: ");
+		float n1 = ler.nextFloat();
+		System.out.println("Informe a nota da prova 2: ");
+		float n2 = ler.nextFloat();
+		System.out.println("Informe a nota da prova 3: ");
+		float n3 = ler.nextFloat();
+		
+		//Determinar a media aritmetica
+		System.out.printf("\nMedia do aluno: %.2f", (n1 + n2 + n3) / 3);
 	}
 
 }
