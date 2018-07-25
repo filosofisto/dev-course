811e11cdf2af69a51d6e2ed87e63ab32fec62b30
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2018-07-25 15:25:47 -0300

    Solução lista03-11

--- Java/Alunos/BrunoPassos/lista03-11/src/lista03/Main.java
+++ Java/Alunos/BrunoPassos/lista03-11/src/lista03/Main.java
@@ -5,36 +5,46 @@ import java.util.Random;
 public class Main {
 
 	/*
-	 * 11. Faça um programa que popule um array de 100 posições com valores
-	 * aleatórios entre 1 e 100, pesquise um número (definido também aleatoriamente)
-	 * dentro do array e caso encontre o número desejado, imprima a posição que o
-	 * mesmo se encontra ou informe que o mesmo não existe. Exiba também o tempo de
+	 * 11. Faï¿½a um programa que popule um array de 100 posiï¿½ï¿½es com valores
+	 * aleatï¿½rios entre 1 e 100, pesquise um nï¿½mero (definido tambï¿½m aleatoriamente)
+	 * dentro do array e caso encontre o nï¿½mero desejado, imprima a posiï¿½ï¿½o que o
+	 * mesmo se encontra ou informe que o mesmo nï¿½o existe. Exiba tambï¿½m o tempo de
 	 * processamento em milissegundos.
 	 */
 	public static void main(String[] args) {
 
-		int[] vetor = new int[100];
+		int[] vetor = new int[100000000];
 		int valoresAleatorios;
 		Random random = new Random();
 		
 		
 		//popula o array
 		for (int i = 0; i < vetor.length; i++) {
-			valoresAleatorios = random.nextInt(100) + 1;
+			valoresAleatorios = random.nextInt(100000000) + 1;
 			vetor[i] = valoresAleatorios;
 		}
 		
 		//realiza busca de um numero aleatoriamente, e verifica se existe o numero
-		for(int i=0; i<vetor.length;i++) {
-			valoresAleatorios = random.nextInt(100) + 1;
+		valoresAleatorios = random.nextInt(100000000) + 1;
+		int posicaoEncontrada = -1;
+		
+		long t1 = System.currentTimeMillis();
+		
+		for(int i=0; i < vetor.length /*&& posicaoEncontrada == -1*/;i++) {
 			if(vetor[i] == valoresAleatorios) {
-				System.out.println("Valor escolhido: " + valoresAleatorios);
-			}else {
-				System.out.println("Numero não encontrado !");
+				posicaoEncontrada = i;
+				break;
 			}
-			
 		}
 		
-	}
+		long t2 = System.currentTimeMillis();
 
+		if (posicaoEncontrada > -1) {
+			System.out.println("Valor encontrado na posicao: " + posicaoEncontrada);
+		} else {
+			System.out.println("Numero nao encontrado !");
+		}
+		
+		System.out.println("Tempo de Processamento: " + (t2-t1) + " ms");
+	}
 }
