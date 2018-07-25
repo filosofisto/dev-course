42ed9f2450e8dfa8520edc91e93160c0efd857be
Renan da Silva <Renan.Silva@cpb.org.br>
2018-07-25 17:30:34 -0300

    12

--- Java/Alunos/Renan/src/Lista03/Campeonato.java
+++ Java/Alunos/Renan/src/Lista03/Campeonato.java
@@ -4,11 +4,18 @@ import java.util.ArrayList;
 
 public class Campeonato {
 	ArrayList<Time> times = new ArrayList<Time>();
-	
-	
+
 	public Campeonato() {
-		
+
+	}
+
+	public Campeonato(ArrayList<Time> times) {
+		this.times = times;
 	}
 	
 	
+	public void criarJogo() {
+		throw new java.lang.UnsupportedOperationException("Ainda não implementado.");
+	}
+
 }
