dfb00872f4c845e3088fa3b35c47dba6570f3b82
Renan da Silva <Renan.Silva@cpb.org.br>
2018-07-26 10:27:23 -0300

    Quadrado com Equals()

--- Java/Exemplos/06-poo-figura/src/main/java/com/javabasico/Quadrado.java
+++ Java/Exemplos/06-poo-figura/src/main/java/com/javabasico/Quadrado.java
@@ -5,4 +5,38 @@ public class Quadrado extends Retangulo {
 	public Quadrado(double lado) {
 		super(lado, lado);
 	}
+
+	
+	public boolean equals(Object object) {
+		if (super.equals(object) && object instanceof Quadrado) {
+			Quadrado outro = (Quadrado) object;
+
+			return outro.getAltura() == this.getAltura();
+
+		} else {
+
+			return false;
+			
+		}
+	}
+
+	public double getLado() {
+		return this.getAltura();
+	}
+
+	public void setLado(double lado) {
+		this.setAltura(lado);
+		this.setBase(lado);
+	}
+
+	public void setAltura(double lado) {
+		this.setAltura(lado);
+		this.setBase(lado);
+	}
+
+	public void setBase(double lado) {
+		this.setAltura(lado);
+		this.setBase(lado);
+	}
+
 }
--- Java/Exemplos/06-poo-figura/src/main/java/com/javabasico/Retangulo.java
+++ Java/Exemplos/06-poo-figura/src/main/java/com/javabasico/Retangulo.java
@@ -20,4 +20,22 @@ public class Retangulo extends Figura {
 		return base*altura;
 	}
 
+	public double getBase() {
+		return base;
+	}
+
+	public void setBase(double base) {
+		this.base = base;
+	}
+
+	public double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	
+	
+
 }
