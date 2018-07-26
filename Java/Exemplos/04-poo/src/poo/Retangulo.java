71a130c538f0d6d0a652b24cf55d33b17e52ce61
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2018-07-25 21:30:33 -0300

    Exemplo 04 POO

---
+++ Java/Exemplos/04-poo/src/poo/Retangulo.java
@@ -0,0 +1,40 @@
+package poo;
+
+public class Retangulo extends Figura {
+
+	double largura;
+	double altura;
+	
+	public Retangulo(double largura, double altura) {
+		setLargura(largura);
+		setAltura(altura);
+	}
+	
+	@Override
+	public double area() {
+		return largura*altura;
+	}
+
+	@Override
+	public double perimetro() {
+		return 2*(largura+altura);
+	}
+
+	public double getLargura() {
+		return largura;
+	}
+
+	public void setLargura(double largura) {
+		this.largura = largura;
+	}
+
+	public double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	
+	
+}
