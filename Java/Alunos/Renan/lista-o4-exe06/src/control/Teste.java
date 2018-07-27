1ab16b6e17c5ead69c9b10bf0f130857e458b73a
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2018-07-27 16:22:34 -0300

    Ajustes

--- Java/Alunos/Renan/lista-o4-exe06/src/control/Teste.java
+++ Java/Alunos/Renan/lista-o4-exe06/src/control/Teste.java
@@ -10,16 +10,18 @@ public class Teste {
 
 	public static void main(String[] args) {
 		
+		System.out.println(Estatistica.mediaAritetica(new double[] {}));
+		
 		double[] vetor = randomFillDouble();
 		
 		for(int i =0 ; i< vetor.length; i++) {
 			out.println("\t["+i+"]\tValor:\t"+vetor[i]);
 		}
 		
-		out.println("\n Estatística deste Vetor: \n");
+		out.println("\n Estatï¿½stica deste Vetor: \n");
 		
-		out.println("\tMédia Aritmética:\t"+Estatistica.mediaAritetica(vetor));
-		out.println("\tMédia Ponderada:\t"+Estatistica.mediaPonderada(vetor, vetor));
+		out.println("\tMï¿½dia Aritmï¿½tica:\t"+Estatistica.mediaAritetica(vetor));
+		out.println("\tMï¿½dia Ponderada:\t"+Estatistica.mediaPonderada(vetor, vetor));
 		out.println("\tMediana:\t"+Estatistica.mediana(vetor));
 		out.println("\tSoma:\t"+Estatistica.soma(vetor));
 		
