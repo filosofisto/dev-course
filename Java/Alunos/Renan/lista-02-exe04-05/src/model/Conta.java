c2b7a78f696028f34aef51f13a9ff35f7c559690
Renan da Silva <Renan.Silva@cpb.org.br>
2018-07-27 15:27:47 -0300

    Exe 05 R.

--- Java/Alunos/Renan/lista-02-exe04-05/src/model/Conta.java
+++ Java/Alunos/Renan/lista-02-exe04-05/src/model/Conta.java
@@ -1,10 +1,14 @@
 package model;
 
+import java.sql.Timestamp;
+import java.util.Calendar;
+
 public class Conta {
 
 	private static int instancias = 0;
 	private static int criadas = 0;
 
+	protected Timestamp criacao;
 	protected long numero;
 	protected Pessoa dono;
 	protected double saldo;
@@ -13,11 +17,19 @@ public class Conta {
 		Conta.criadas++;
 		Conta.instancias++;
 	}
-
-	public Conta(long numero, Pessoa dono, double valor) {
+	
+	public Conta(long numero, Pessoa dono) {
+		Calendar calendar = Calendar.getInstance();
+		java.util.Date now = calendar.getTime();
+		this.criacao = new java.sql.Timestamp(now.getTime());
+		Conta.criadas++;
+		Conta.instancias++;
+	}
+	
+	public Conta(long numero, Pessoa dono, Timestamp criacao) {
 		this.numero = numero;
 		this.dono = dono;
-		this.saldo = valor;
+		this.criacao = criacao;
 		Conta.criadas++;
 		Conta.instancias++;
 	}
