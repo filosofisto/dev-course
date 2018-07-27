610be523caf6342000b1763d6d04f095c2bb011a
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2018-07-27 17:50:41 -0300

    Ajustes

--- Java/Alunos/Lenilson/lista-04-05/src/lista04/Conta.java
+++ Java/Alunos/Lenilson/lista-04-05/src/lista04/Conta.java
@@ -1,12 +1,17 @@
 package lista04;
 
 public class Conta {
+	
 	private int numero;
 	private double saldo;
+	private Operacao[] operacoes;
+	private int index;
 	
 	public Conta(int numero) {
 		setNumero(numero);
 		saldo = 0;
+		operacoes = new Operacao[10];
+		index = -1;
 	}
 
 	public int getNumero() {
@@ -22,15 +27,26 @@ public class Conta {
 	}
 
 	public void deposita (double valor) {
-		this.saldo += valor;
+		double saldoFinal = saldo + valor;
+		Operacao operacao = new Operacao(TipoOperacao.DEPOSITA, saldo, saldoFinal);
 		
+		this.saldo = saldoFinal;
+		adicionarOperacao(operacao);
+	}
+
+	private void adicionarOperacao(Operacao operacao) {
+		operacoes[++index] = operacao;
 	}
 	
-	public void retira (double valor) throws Exception{
+	public void retira (double valor) throws Exception {
 		if (this.saldo < valor) {
-			throw new Exception();
+			throw new Exception("Saldo insuficiente");
 		}
-		this.saldo -= valor;
+		
+		double saldoFinal = saldo - valor;
+		Operacao operacao = new Operacao(TipoOperacao.RETIRA, saldo, saldoFinal);
+		this.saldo = saldoFinal;
+		adicionarOperacao(operacao);
 	}
 	
 	public void transfere (double valor, Conta contaDestino) throws Exception{
@@ -40,5 +56,11 @@ public class Conta {
 	
 	public void saldoAtual() {
 		System.out.printf("Conta: %d Saldo Atual: R$ %.2f\n", numero, saldo);
+	}
+	
+	public void extrato() {
+		for (Operacao op: operacoes) {
+			System.out.println(op);
+		}
 	}
 }
