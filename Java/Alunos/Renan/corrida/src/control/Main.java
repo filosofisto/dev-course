40499e03539792540633887d99874a1dd36d5ed5
Renan da Silva <Renan.Silva@cpb.org.br>
2018-08-03 12:00:14 -0300

    R.

--- Java/Alunos/Renan/corrida/src/control/Main.java
+++ Java/Alunos/Renan/corrida/src/control/Main.java
@@ -9,26 +9,26 @@ public class Main {
 
 	private static Corredor[] corredores;
 	private static int chegada;
-	
+
 	public static void main(String[] args) {
 
 		Random r = new Random();
-		chegada = r.nextInt(100)+25;
-		
-		
-		
-		System.out.println("\n\t Que comece a corrida de "+chegada+" unidades!");
-		corredores = instaciarCorredor(chegada, r.nextInt(10)+3, true);
+		chegada = r.nextInt(100) + 25;
+
+		System.out.println("\n\t Que comece a corrida de " + chegada + " unidades!");
+		corredores = instaciarCorredor(r.nextInt(10) + 3, chegada, true);
 	}
-	
+
 	private static Corredor[] instaciarCorredor(int quantidade, int chegada, boolean teraoSono) {
 		Corredor[] corredores = new Corredor[quantidade];
-		
-		for(int i =0; i < corredores.length; i++) {
-			corredores[i] =  new Corredor(chegada, teraoSono);
+
+		for (int i = 0; i < corredores.length; i++) {
+			corredores[i] = new Corredor(chegada, teraoSono);
 			corredores[i].start();
 		}
 		return corredores;
 	}
+	
+	
 
 }
--- Java/Alunos/Renan/corrida/src/model/Corredor.java
+++ Java/Alunos/Renan/corrida/src/model/Corredor.java
@@ -2,56 +2,55 @@ package model;
 
 import java.util.Random;
 
-public class Corredor extends Thread{
-	
+public class Corredor extends Thread {
+
 	private static int contador;
-	
+
 	private int chegada;
 	private int distanciaPercorrida;
+	private long tempoDeCorrida;
+	
 	
 	private boolean comSono;
-	
-	
+
 	public Corredor(int chegada, boolean comSono) {
 		this.chegada = chegada;
 		this.comSono = comSono;
-		this.distanciaPercorrida = 0;
 		
+		this.distanciaPercorrida = 0;
+		this.tempoDeCorrida = 0;
+
 		Corredor.contador++;
 	}
-	
-	public Corredor(int chegada){
+
+	public Corredor(int chegada) {
 		this(chegada, false);
 	}
-	
-	
-	
+
 	public void run() {
-		if(this.chegada == 0) {
-			try {
-				throw new Exception("Chegada igual a zero");
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
+		
+		long inicio = System.currentTimeMillis();
+		
 		Random r = new Random();
-		while(this.distanciaPercorrida < this.chegada) {
-			if(this.comSono && r.nextBoolean()) {
+		while (this.distanciaPercorrida < this.chegada) {
+			if (this.comSono && r.nextBoolean()) {
 				try {
-					Thread.sleep(r.nextInt(10)+1);
+					Thread.sleep(r.nextInt(10) + 1);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
 			}
-			
+
 			++this.distanciaPercorrida;
-			System.out.println("\t Corredor de ID nº"+this.getId()+"\t percorreu "+this.distanciaPercorrida+", faltando "+(this.chegada-this.distanciaPercorrida)+" para a chegada em sua corrida de "+this.chegada+".");
+			System.out.println("\t Corredor de ID nº" + this.getId() + "\t percorreu " + this.distanciaPercorrida
+					+ ",\t faltando " + (this.chegada - this.distanciaPercorrida) + " para a chegada em sua corrida de "
+					+ this.chegada + ".");
 		}
+		long fim = System.currentTimeMillis();
 		
-		System.out.println("\t Corredor de ID nº"+this.getId()+"\t chegou na chegada!");
+		this.tempoDeCorrida = fim - inicio;
+
+		System.out.println("\t Corredor de ID nº" + this.getId() + "\t CHEGOU na linha de chegada levando "+this.tempoDeCorrida+"ms!");
 	}
-	
-	
-	
-	
+
 }
