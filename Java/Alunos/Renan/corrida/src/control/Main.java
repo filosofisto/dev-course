99ad925634a72a82cdbd0473d781ccd3fd522c8b
Renan da Silva <Renan.Silva@cpb.org.br>
2018-08-03 13:17:52 -0300

    CORRIDA PRONTA

--- Java/Alunos/Renan/corrida/src/control/Main.java
+++ Java/Alunos/Renan/corrida/src/control/Main.java
@@ -1,5 +1,6 @@
 package control;
 
+import java.util.Arrays;
 import java.util.Random;
 
 import model.Corredor;
@@ -10,15 +11,27 @@ public class Main {
 	private static Corredor[] corredores;
 	private static int chegada;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		Random r = new Random();
 		chegada = r.nextInt(100) + 25;
 
 		System.out.println("\n\t Que comece a corrida de " + chegada + " unidades!");
 		corredores = instaciarCorredor(r.nextInt(10) + 3, chegada, true);
+
 		
+
+		while(!isCorridaFinalizada(corredores)) {
+			Thread.sleep(100);
+		}
 		
+		System.out.println("\n\t Fim da Corrida!");
+		Arrays.sort(corredores);
+		
+		System.out.println("\n\t Ordem de chegada: ");
+		for (Corredor c : corredores) {
+			out.println(c.toString());
+		}
 	}
 
 	private static Corredor[] instaciarCorredor(int quantidade, int chegada, boolean teraoSono) {
@@ -30,7 +43,18 @@ public class Main {
 		}
 		return corredores;
 	}
-	
-	
+
+	private static boolean isCorridaFinalizada(Corredor... corredores) {
+		boolean terminou = true;
+
+		for (Corredor c : corredores) {
+			if (!c.chegou()) {
+				terminou = false;
+				break;
+			}
+		}
+
+		return terminou;
+	}
 
 }
--- Java/Alunos/Renan/corrida/src/model/Corredor.java
+++ Java/Alunos/Renan/corrida/src/model/Corredor.java
@@ -1,20 +1,28 @@
 package model;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
 
-public class Corredor extends Thread implements Comparable<Corredor>{
+/**
+ * @author CPB
+ *
+ */
+public class Corredor extends Thread implements Comparable<Corredor> {
 
 	protected int chegada;
 	protected int distanciaPercorrida;
 	protected long tempoDeCorrida;
-	
-	
+
 	protected boolean comSono;
 
 	public Corredor(int chegada, boolean comSono) {
 		this.chegada = chegada;
 		this.comSono = comSono;
-		
+
 		this.distanciaPercorrida = 0;
 		this.tempoDeCorrida = 0;
 	}
@@ -24,9 +32,9 @@ public class Corredor extends Thread implements Comparable<Corredor>{
 	}
 
 	public void run() {
-		
+
 		long inicio = System.currentTimeMillis();
-		
+
 		Random r = new Random();
 		while (this.distanciaPercorrida < this.chegada) {
 			if (this.comSono && r.nextBoolean()) {
@@ -43,25 +51,77 @@ public class Corredor extends Thread implements Comparable<Corredor>{
 					+ this.chegada + ".");
 		}
 		long fim = System.currentTimeMillis();
-		
+
 		this.tempoDeCorrida = fim - inicio;
 
-		System.out.println("\t Corredor de ID nº" + this.getId() + "\t CHEGOU na linha de chegada levando "+this.tempoDeCorrida+"ms!");
+		System.out.println("\t Corredor de ID nº" + this.getId() + "\t CHEGOU na linha de chegada levando "
+				+ this.tempoDeCorrida + "ms!");
 	}
-	
+
 	public boolean chegou() {
 		return this.distanciaPercorrida >= this.chegada;
 	}
 
 	@Override
 	public int compareTo(Corredor outro) {
-		if(outro.chegou() && this.chegou()) {
-			return (int) (outro.tempoDeCorrida - this.tempoDeCorrida);
+		int ret = 0;
+		if (outro.chegou() && this.chegou()) {
+			// Compara pelo tempo de corrida (SE ambos terminaram a corrida)
+			ret = (int) (this.tempoDeCorrida - outro.tempoDeCorrida);
 		}
-		
-		return outro.distanciaPercorrida - this.distanciaPercorrida; 
-	}
-	
-	
 
+		if (ret == 0) {
+			// Compara pela distanciaPercorrida (SE a distanciaPercorrida for diferente um
+			// do outro)
+			ret = outro.distanciaPercorrida - this.distanciaPercorrida;
+			if (ret == 0) {
+				//	Comprara pela tamanho da corrida	(SE as distancias percorridas fossem a mesma)
+				ret = outro.chegada - this.chegada;
+				if(ret == 0) {
+					// Compara pelo ID (SE nenhuma das condições anteriores forem satisfeitas)
+					ret = (int) (this.getId() - outro.getId());
+				}
+			}
+		}
+
+		return ret;
+	}
+
+	@Override
+	public String toString() {
+		return "Corredor [id =" + this.getId() + ", chegada=" + chegada + ", distanciaPercorrida=" + distanciaPercorrida
+				+ ", tempoDeCorrida=" + tempoDeCorrida + ", comSono=" + comSono + "]";
+	}
+
+	public int getChegada() {
+		return chegada;
+	}
+
+	public void setChegada(int chegada) {
+		this.chegada = chegada;
+	}
+
+	public int getDistanciaPercorrida() {
+		return distanciaPercorrida;
+	}
+
+	public void setDistanciaPercorrida(int distanciaPercorrida) {
+		this.distanciaPercorrida = distanciaPercorrida;
+	}
+
+	public long getTempoDeCorrida() {
+		return tempoDeCorrida;
+	}
+
+	public void setTempoDeCorrida(long tempoDeCorrida) {
+		this.tempoDeCorrida = tempoDeCorrida;
+	}
+
+	public boolean isComSono() {
+		return comSono;
+	}
+
+	public void setComSono(boolean comSono) {
+		this.comSono = comSono;
+	}
 }
