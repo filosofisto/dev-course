07ab493992e55be98084d72b72b06c29b259fcf1
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2018-08-04 11:50:15 -0300

    Ajustes

--- Java/Alunos/Renan/prjUtilizandoSax/src/main/java/model/xml/ContasContentHandler.java
+++ Java/Alunos/Renan/prjUtilizandoSax/src/main/java/model/xml/ContasContentHandler.java
@@ -13,14 +13,10 @@ import org.xml.sax.helpers.DefaultHandler;
 import model.Conta;
 
 public class ContasContentHandler extends DefaultHandler {
-	enum Inside {
-		BANCO, AGENCIA, CPF, SALDO, NONE
-	};
 
 	private long t1, t2;
 	private List<Conta> contas;
 	private Conta conta;
-	private Inside inside;
 
 	public List<Conta> getContas() {
 		return contas;
@@ -41,7 +37,6 @@ public class ContasContentHandler extends DefaultHandler {
 	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
 		if ("contas".equals(localName)) {
 			contas = new ArrayList<Conta>();
-			inside = Inside.NONE;
 		} else if ("conta".equals(localName)) {
 			conta = new Conta();
 			conta.setBanco(attributes.getValue("banco"));
@@ -49,46 +44,6 @@ public class ContasContentHandler extends DefaultHandler {
 			conta.setCpf(attributes.getValue("cpf"));
 			conta.setSaldo(Double.parseDouble(attributes.getValue("saldo")));
 			contas.add(conta);
-			
-		} else if ("banco".equals(localName)) {
-			inside = Inside.BANCO;
-		} else if ("agencia".equals(localName)) {
-			inside = Inside.AGENCIA;
-		} else if ("cpf".equals(localName)) {
-			inside = Inside.CPF;
-		} else if ("saldo".equals(localName)) {
-			inside = Inside.SALDO;
-		} else {
-			inside = Inside.NONE;
 		}
-	}
-
-	@Override
-	public void characters(char[] ch, int start, int length) throws SAXException {
-		String content = new String(ch, start, length);
-		if (isContentInvalid(content)) {
-			return;
-		}
-
-		switch (inside) {
-		case BANCO:
-			conta.setBanco(content);
-			break;
-		case AGENCIA:
-			conta.setAgencia(content);
-			break;
-		case CPF:
-			conta.setCpf(content);
-			break;
-		case SALDO:
-			conta.setSaldo(Double.parseDouble(content));
-			break;
-		}
-	}
-
-	private boolean isContentInvalid(String content) {
-		// return content == null || content.trim().isEmpty() || content.indexOf("\n") >
-		// -1 || content.indexOf("\t") > -1;
-		return true; // por enquanto ...
 	}
 }
