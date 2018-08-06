479e44260d875df80a66f49db7a4adc3b31c53bf
Bruno Passos <brunopassos133@hotmail.com>
2018-08-06 09:36:47 -0300

    ajustes

--- Java/Alunos/BrunoPassos/exer-sax2/src/main/java/cursojava/sax/ContasContentHandler.java
+++ Java/Alunos/BrunoPassos/exer-sax2/src/main/java/cursojava/sax/ContasContentHandler.java
@@ -12,72 +12,40 @@ import org.xml.sax.helpers.DefaultHandler;
 
 public class ContasContentHandler extends DefaultHandler {
 
-    enum Inside { BANCO, AGENCIA, CPF, SALDO, NONE };
+	private long t1, t2;
+	private List<Conta> contas;
+	private Conta conta;
 
-    private long t1, t2;
-    private List<Conta> contas;
-    private Conta conta;
-    private Inside inside;
+	public List<Conta> getContas() {
+		return contas;
+	}
 
-    public List<Conta> getContas() {
-        return contas;
-    }
+	@Override
+	public void startDocument() throws SAXException {
+		t1 = currentTimeMillis();
+	}
 
-    @Override
-    public void startDocument() throws SAXException {
-        t1 = currentTimeMillis();
-    }
+	@Override
+	public void endDocument() throws SAXException {
+		t2 = currentTimeMillis();
+		out.printf("Tempo de processamento: %d ms\n", (t2 - t1));
+	}
 
-    @Override
-    public void endDocument() throws SAXException {
-        t2 = currentTimeMillis();
-        out.printf("Tempo de processamento: %d ms\n", (t2-t1));
-    }
+	@Override
+	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+		if ("contas".equals(localName)) {
+			contas = new ArrayList<Conta>();
+		} else if ("conta".equals(localName)) {
+			conta = new Conta();
+			conta.setCpf(attributes.getValue("cpf"));
+			conta.setBanco(attributes.getValue("banco"));
+			conta.setAgencia(attributes.getValue("agencia"));
+			conta.setSaldo(Double.parseDouble(attributes.getValue("saldo")));
+			contas.add(conta);
+		} 
+	}
 
-    @Override
-    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-        if ("contas".equals(localName)) {
-            contas = new ArrayList<Conta>();
-        } else if ("conta".equals(localName)) {
-            conta = new Conta();
-            contas.add(conta);
-        } else if ("banco".equals(localName)) {
-        	conta.setBanco(attributes.getValue("banco"));
-        } else if ("agencia".equals(localName)) {
-        	conta.setAgencia(attributes.getValue("agencia"));
-        } else if ("cpf".equals(localName)) {
-        	conta.setCpf(attributes.getValue("cpf"));
-        } else if ("saldo".equals(localName)) {
-        	conta.setSaldo(Double.parseDouble(attributes.getValue("saldo")));
-        } else {
-            inside = Inside.NONE;
-        }
-    }
-
-    @Override
-    public void characters(char[] ch, int start, int length) throws SAXException {
-        String content = new String(ch, start, length);
-        if (isContentInvalid(content)) {
-            return;
-        }
-
-        switch (inside) {
-            case BANCO:
-                conta.setBanco(content);
-                break;
-            case AGENCIA:
-                conta.setAgencia(content);
-                break;
-            case CPF:
-                conta.setCpf(content);
-                break;
-            case SALDO:
-                conta.setSaldo(Double.parseDouble(content));
-                break;
-        }
-    }
-
-    private boolean isContentInvalid(String content) {
-        return content == null || content.trim().isEmpty() || content.indexOf("\n") > -1 || content.indexOf("\t") > -1;
-    }
+	private boolean isContentInvalid(String content) {
+		return content == null || content.trim().isEmpty() || content.indexOf("\n") > -1 || content.indexOf("\t") > -1;
+	}
 }
