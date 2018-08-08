7dd6542ef5304c8f1bd1c29adfdaa6805e5e0afe
Renan da Silva <Renan.Silva@cpb.org.br>
2018-08-08 18:04:06 -0300

    Agora vai!

---
+++ Java/Exemplos/125-jeeapp/125-jeeapp-web/src/main/java/com/cursojava/controller/PessoaController.java
@@ -0,0 +1,132 @@
+package com.cursojava.controller;
+
+import com.cursojava.dominio.Aeroporto;
+import com.cursojava.servico.AeroportoServico;
+import com.cursojava.util.lazydatamodel.AeroportoLazyDataModel;
+
+import javax.enterprise.context.Conversation;
+import javax.enterprise.context.ConversationScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named
+@ConversationScoped
+public class PessoaController implements Serializable {
+
+	private static final long serialVersionUID = -5129718892570710240L;
+
+	@Inject
+	protected Conversation conversation;
+
+	@Inject
+	protected FacesContext facesContext;
+
+	@Inject
+	private AeroportoServico aeroportoServico;
+
+	@Inject
+	private AeroportoLazyDataModel aeroportosDataModel;
+
+	private Aeroporto aeroporto;
+
+	public String begin() {
+		conversation.begin();
+
+		return "listaPessoas";
+	}
+
+	public String end() {
+		conversation.end();
+
+		return "home";
+	}
+
+    public Aeroporto getAeroporto() {
+        return aeroporto;
+    }
+
+    public String novoAeroporto() {
+	    setAeroporto(new Aeroporto());
+
+	    return "novoPessoa";
+    }
+
+    public void incluir() {
+		try {
+			aeroportoServico.incluir(aeroporto);
+
+			// Prepara para cadastrar outro aeroporto
+			setAeroporto(new Aeroporto());
+
+			messageSucesso("Operação realizada com sucesso");
+
+			aeroportosDataModel.forceRefresh();
+		} catch (Exception e) {
+			messageException(e);
+		}
+	}
+
+    public String atualizar() {
+		try {
+			aeroportoServico.atualizar(aeroporto);
+			messageSucesso("Operação realizada com sucesso");
+
+			aeroportosDataModel.forceRefresh();
+
+			return "listaAeroportos";
+		} catch (Exception e) {
+			messageException(e);
+			return null;
+		}
+    }
+
+    public String cancelarEdicao() {
+        aeroportosDataModel.forceRefresh();
+
+        return "listaAeroportos";
+    }
+
+	/*@Produces
+	@Named
+	public List<Aeroporto> getAeroportos() {
+		try {
+			return aeroportoServico.listar();
+		} catch (Exception e) {
+			messageException(e);
+			return null;
+		}
+	}*/
+
+	private void messageException(Exception e) {
+		FacesMessage m = new FacesMessage(
+				FacesMessage.SEVERITY_ERROR,
+				e.getMessage(),
+				"Atualização falhou"
+		);
+		this.facesContext.addMessage(null, m);
+		this.facesContext.validationFailed();
+	}
+
+	private void messageSucesso(String mensagem) {
+		FacesMessage m = new FacesMessage(
+				FacesMessage.SEVERITY_INFO,
+				mensagem, null
+		);
+		this.facesContext.addMessage(null, m);
+	}
+
+	public void setAeroporto(Aeroporto aeroporto) {
+		this.aeroporto = aeroporto;
+	}
+
+    public AeroportoLazyDataModel getAeroportosDataModel() {
+        return aeroportosDataModel;
+    }
+
+    public void setAeroportosDataModel(AeroportoLazyDataModel aeroportosDataModel) {
+        this.aeroportosDataModel = aeroportosDataModel;
+    }
+}
